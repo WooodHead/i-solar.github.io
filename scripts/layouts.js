d5901c2b8448d7d905c8bc84b6ae4d4a6cae318a
solar <love0and0life@gmail.com>
2016-05-06 14:21:59 +0800

    改版首次提交

---
+++ scripts/layouts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var socialPanel = document.querySelector('.socialPanel');
+    // 绑定显示图标信息事件
+    socialPanel.addEventListener('mouseover', function function_name (argument) {
+         // body...  
+    });
+});
